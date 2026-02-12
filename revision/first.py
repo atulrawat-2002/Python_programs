fd9cf1a4997d2658747a27213a9e91a794c23445
Atul <dr6217811@gmail.com>
2026-02-12 17:02:31 +0530

    Binary types in python

---
+++ revision/first.py
@@ -0,0 +1,55 @@
+# # def greet(greeting):
+# #     print(greeting)
+
+
+# # # greet("Good Morning")
+
+
+# # x = [9, 8, 7]
+# # y = x
+
+# # del x
+  
+# # print(y)
+
+# a = 9
+# b = True
+
+# print(2 == b)
+
+# c = 8 + 8j
+
+# print( type(c) )
+
+
+# str = "Atul"
+# print(str, type(type(str)))
+
+# ls = [9, 8, 7, 0]
+# print(ls)
+
+# tpl = (9, 8, 7, 0)
+# print(tpl[0])
+
+
+# print(range(9, 89))
+
+
+# obj = {
+#     "name": "Atul",
+#     "age": 23,
+#     "yes" : True,
+#     "skills": (3, 3, 3, ),
+#     "sjkdf": [8, 9, 9, ],
+#     "sdf": range(4, 4)
+# }
+# # print(obj)
+
+
+# s = {3, 3, 33}
+# s.add(4)
+# print(s)
+
+b = bytearray(b"hello")
+
+print(b)
